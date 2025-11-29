571579a91656c011f6df1f94557492bdc02ecd31
Bunny <bunny@gmail.com>
2025-11-29 15:39:37 +0800

    another fix bug that my member see that in the admin about the company name not fetch

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,12 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   build: {
+    outDir: "dist",
     rollupOptions: {
       output: {
+        entryFileNames: "assets/[name]-[hash].js",
+        chunkFileNames: "assets/[name]-[hash].js",
+        assetFileNames: "assets/[name]-[hash][extname]",
         manualChunks(id) {
           if (id.includes("node_modules")) {
             return id
